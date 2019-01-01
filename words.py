b74f80f8e8776dd0e38de67ae9efcd80921472c2
Pasan Premaratne <pasan.premaratne@live.com>
2019-01-01 00:41:48 +0000

    Some more stuff

--- words.py
+++ words.py
@@ -1,12 +1,18 @@
 from urllib.request import urlopen
 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words = []
-    for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
-            story_words.append(word)
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
 
-            for word in story_words:
-                print(word)
+def print_words(story_words):
+    for word in story_words:
+        print(word)
 
+if __name__ == '__main__':
+    words = fetch_words()
+    print_words(words)
