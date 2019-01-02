728f47ee43e4455e7094ce032240436c32300e31
Pasan Premaratne <pasan@pasandev.com>
2019-01-02 08:35:21 +0000

    updated after Modularity/Main functions lesson but does not work now

--- words.py
+++ words.py
@@ -1,7 +1,8 @@
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
@@ -9,10 +10,13 @@ def fetch_words():
                 story_words.append(word)
     return story_words
 
-def print_words(story_words):
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
+
+def main(url):
+    words = fetch_words(url)
+    print_items(words)
 
 if __name__ == '__main__':
-    words = fetch_words()
-    print_words(words)
+    main(sys.argv[1])
