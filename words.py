22ac104b451a3a46c848c28da398696ff64ce83b
Pasan Premaratne <pasan@pasandev.com>
2019-01-02 13:42:16 +0000

    updated after Modularity/Main functions lesson but does not work now

--- words.py
+++ words.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python3
+"""
+Retrieve and print words from a URL.
+
+Usage:
+python3 words.py <URL>
+"""
+
+
 import sys
 from urllib.request import urlopen
 
 def fetch_words(url):
+    """Fetch a list of words form a URl.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from the document.
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -11,12 +28,26 @@ def fetch_words(url):
     return story_words
 
 def print_items(items):
+    """
+    Print items one per line.
+    :param items:
+        An interable series of printable items.
+    :return:
+        None
+    """
     for item in items:
         print(item)
 
 def main(url):
+    """
+    Print each word from a text document from a URL.
+    :param url:
+        The URL of a UTF-8 document.
+    :return:
+        None
+    """
     words = fetch_words(url)
     print_items(words)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1]) #The 0th arg is the module filename
